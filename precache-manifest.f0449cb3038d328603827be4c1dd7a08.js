37955025f8b5b2d39e3b86085da7a26999fba12b
Vladislav Safonichev <unsviperm@gmail.com>
2020-02-26 01:27:20 +0300

    Updates

---
+++ precache-manifest.f0449cb3038d328603827be4c1dd7a08.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1abcb24a5a7f815c635d777ee3a0e688",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dca4db01c5d8e9f464e3",
+    "url": "/static/css/2.1cc9201e.chunk.css"
+  },
+  {
+    "revision": "90c49e4e7fac849982a4",
+    "url": "/static/css/main.30e6b870.chunk.css"
+  },
+  {
+    "revision": "dca4db01c5d8e9f464e3",
+    "url": "/static/js/2.8c13e87c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.8c13e87c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90c49e4e7fac849982a4",
+    "url": "/static/js/main.a1e15289.chunk.js"
+  },
+  {
+    "revision": "1745a6e760f544fa7934",
+    "url": "/static/js/runtime-main.f1de9b7e.js"
+  },
+  {
+    "revision": "e55e6834a1d57a1bc3290247836cef98",
+    "url": "/static/media/background.e55e6834.jpg"
+  },
+  {
+    "revision": "5f85a05aad8ca4d8b1c0755fb2be91d2",
+    "url": "/static/media/calculating.5f85a05a.svg"
+  },
+  {
+    "revision": "bdf5bbd371cc7b79fa590f3d7497c0d7",
+    "url": "/static/media/cleaning.bdf5bbd3.svg"
+  },
+  {
+    "revision": "243b7d3dcbc235d9b973b3116ddbb703",
+    "url": "/static/media/cup.243b7d3d.svg"
+  },
+  {
+    "revision": "07e9fc01904383928927546ab214e0c5",
+    "url": "/static/media/drill_1.07e9fc01.jpg"
+  },
+  {
+    "revision": "9f67deebb7994db87b8c5df0ecdd6e4a",
+    "url": "/static/media/drill_1.9f67deeb.jpg"
+  },
+  {
+    "revision": "736865d86a2dbb31a8f092d81f1794cc",
+    "url": "/static/media/drill_2.736865d8.jpg"
+  },
+  {
+    "revision": "937817eca7a9485be3a232f953dd9128",
+    "url": "/static/media/drill_2.937817ec.jpg"
+  },
+  {
+    "revision": "427c4d1a5bbd644fafcffb1e7803ca8a",
+    "url": "/static/media/drill_3.427c4d1a.jpg"
+  },
+  {
+    "revision": "ee8fd63f144773c045251a1f29a7153b",
+    "url": "/static/media/drill_3.ee8fd63f.jpg"
+  },
+  {
+    "revision": "2281a05cf807e6fa5107fa26aea71a7d",
+    "url": "/static/media/drill_4.2281a05c.jpg"
+  },
+  {
+    "revision": "92d6d4b9f7de71d670827502b4037de3",
+    "url": "/static/media/drill_4.92d6d4b9.jpg"
+  },
+  {
+    "revision": "4832b11b8743d090d5f727c65cdcd350",
+    "url": "/static/media/drill_5.4832b11b.jpg"
+  },
+  {
+    "revision": "e9d72d154392be1644cca45cb161e84d",
+    "url": "/static/media/drill_5.e9d72d15.jpg"
+  },
+  {
+    "revision": "f0de1c03a3af96762e699e59e9512a48",
+    "url": "/static/media/ruler.f0de1c03.svg"
+  },
+  {
+    "revision": "355ddd2ba14e8ad3460a66f787927be7",
+    "url": "/static/media/vacuum-cleaner.355ddd2b.svg"
+  },
+  {
+    "revision": "16e7608a0ff211fc6690e55a60ef49dd",
+    "url": "/static/media/wall.16e7608a.svg"
+  },
+  {
+    "revision": "770022a2d81c29822c3203a8e261a9b7",
+    "url": "/static/media/window.770022a2.svg"
+  }
+]);
